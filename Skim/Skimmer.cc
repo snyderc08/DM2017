e9d7df6fcd30bf77397af5912768b85c21f63d28
abdollah <smohamadi@gmail.com>
2017-03-15 09:39:04 -0500

    update skimmer MET80 Iso_1.0 Id_Tight

--- Skim/Skimmer.cc
+++ Skim/Skimmer.cc
@@ -82,7 +82,7 @@ void Skimmer::Loop(TString outputName, int skm)
         TLorentzVector Mu4Momentum, Jet4Momentum;
         
   
-        if(pfMET < 100) continue;
+        if(pfMET < 80) continue;
         hcount->Fill(3);
 
         //MuJet
@@ -90,7 +90,7 @@ void Skimmer::Loop(TString outputName, int skm)
             float IsoMu=muPFChIso->at(imu)/muPt->at(imu);
             if ( (muPFNeuIso->at(imu) + muPFPhoIso->at(imu) - 0.5* muPFPUIso->at(imu) )  > 0.0)
                 IsoMu= ( muPFChIso->at(imu)/muPt->at(imu) + muPFNeuIso->at(imu) + muPFPhoIso->at(imu) - 0.5* muPFPUIso->at(imu))/muPt->at(imu);
-            if (muPt->at(imu) > 60 &&  fabs(muEta->at(imu)) < 2.4 &&  IsoMu < 0.50 ){
+            if (muPt->at(imu) > 60 && (muIDbit->at(imu) >> 2 & 1) && fabs(muEta->at(imu)) < 2.4 &&  IsoMu < 1.0 ){
                 Mu4Momentum.SetPtEtaPhiM(muPt->at(imu),muEta->at(imu),muPhi->at(imu),MuMass);
                 
                 for (int ijet = 0; ijet < nJet; ++ijet){
