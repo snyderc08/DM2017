13cd4207c0a6398aca4f8b8819eb3731729578f6
abdollah <smohamadi@gmail.com>
2017-08-14 12:49:54 +0200

    ‘update’

---
+++ RecoStudy/RunFullSamples_KFactor.sh
@@ -0,0 +1,10 @@
+./Make.sh CodexAnalyzer_Preselection.cc
+
+
+
+
+./CodexAnalyzer_Preselection.exe  OutFiles_FXFX/DYJetsToLL_FXFX.root ../ROOT80X/FXFX/DYJetsToLL*FXFX*.root
+
+./CodexAnalyzer_Preselection.exe  OutFiles_FXFX/WJetsToLNu_FXFX.root ../ROOT80X/FXFX/WJetsToLNu*FXFX*.root
+
+
